8020b001e969c35d11da7e1b74fed86b03f0b3e7
John Gomez <jgomez818@gmail.com>
2015-08-11 21:15:00 -0400

    added working accelerometer w/ values

--- MomenTone/app/src/main/java/lighterletter/c4q/nyc/momentone/MainActivity.java
+++ MomenTone/app/src/main/java/lighterletter/c4q/nyc/momentone/MainActivity.java
@@ -1,9 +1,11 @@
 package lighterletter.c4q.nyc.momentone;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+    }
+
+    public void accelerometerActivity(View v){
+        Intent i = new Intent(this, Accelerometer.class);
+        startActivity(i);
     }
 
     @Override
