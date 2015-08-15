2a40eb03a03ca4463a54e33d6449b8b16c2432b0
John Gomez <jgomez818@gmail.com>
2015-08-15 14:06:28 -0400

    audio frequency test

--- MomenTone/app/src/main/java/lighterletter/c4q/nyc/momentone/MainActivity.java
+++ MomenTone/app/src/main/java/lighterletter/c4q/nyc/momentone/MainActivity.java
@@ -1,46 +1,137 @@
 package lighterletter.c4q.nyc.momentone;
 
 import android.content.Intent;
+import android.media.AudioFormat;
+import android.media.AudioManager;
+import android.media.AudioTrack;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
 
+    Thread t;
+    int sr = 44100;
+    boolean isRunning = true;
+    SeekBar fSlider;
+    double sliderval;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // point the slider to thwe GUI widget
+        fSlider = (SeekBar) findViewById(R.id.main_textview);
 
+        // create a listener for the slider bar;
+        SeekBar.OnSeekBarChangeListener listener = new SeekBar.OnSeekBarChangeListener() {
+            public void onStopTrackingTouch(SeekBar seekBar) {
 
-    }
+            }
+            public void onStartTrackingTouch(SeekBar seekBar) {
 
-    public void accelerometerActivity(View v){
-        Intent i = new Intent(this, Accelerometer.class);
-        startActivity(i);
+            }
+            public void onProgressChanged(SeekBar seekBar,
+                                          int progress,
+                                          boolean fromUser) {
+                if(fromUser) sliderval = progress / (double)seekBar.getMax();
+            }
+        };
+
+        // set the listener on the slider
+        fSlider.setOnSeekBarChangeListener(listener);
+
+        // start a new thread to synthesise audio
+        t = new Thread() {
+            public void run() {
+                // set process priority
+                setPriority(Thread.MAX_PRIORITY);
+                // set the buffer size
+                int buffsize = AudioTrack.getMinBufferSize(sr,
+                        AudioFormat.CHANNEL_OUT_MONO, AudioFormat.ENCODING_PCM_16BIT);
+                // create an audiotrack object
+                AudioTrack audioTrack = new AudioTrack(AudioManager.STREAM_MUSIC,
+                        sr, AudioFormat.CHANNEL_OUT_MONO,
+                        AudioFormat.ENCODING_PCM_16BIT, buffsize,
+                        AudioTrack.MODE_STREAM);
+
+                short samples[] = new short[buffsize];
+                int amp = 10000;
+                double twopi = 8.*Math.atan(1.);
+                double fr = 440.f;
+                double ph = 0.0;
+
+                // start audio
+                audioTrack.play();
+
+                // synthesis loop
+                while(isRunning){
+                    fr =  440 + 440*sliderval;
+                    for(int i=0; i < buffsize; i++){
+                        samples[i] = (short) (amp*Math.sin(ph));
+                        ph += twopi*fr/sr;
+                    }
+                    audioTrack.write(samples, 0, buffsize);
+                }
+                audioTrack.stop();
+                audioTrack.release();
+            }
+        };
+        t.start();
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    public void onDestroy(){
+        super.onDestroy();
+        isRunning = false;
+        try {
+            t.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
-
-        return super.onOptionsItemSelected(item);
+        t = null;
     }
+
+
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//
+//    }
+//
+    public void accelerometerActivity(View v){
+        Intent i = new Intent(this, Accelerometer.class);
+        startActivity(i);
+    }
+//
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.menu_main, menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        int id = item.getItemId();
+//
+//        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+//
+//        return super.onOptionsItemSelected(item);
+//    }
 }
