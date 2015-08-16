e1d97690dd5c157b451a14a7ae98ebc90456e3d1
Jorge Reina <jorgereina1986@gmail.com>
2015-08-16 15:59:49 -0400

    created main activity ui

--- MomenTone/app/src/main/java/lighterletter/c4q/nyc/momentone/MainActivity.java
+++ MomenTone/app/src/main/java/lighterletter/c4q/nyc/momentone/MainActivity.java
@@ -4,13 +4,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView logoButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
     @Override
